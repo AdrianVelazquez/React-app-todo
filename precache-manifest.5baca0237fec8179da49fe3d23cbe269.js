65719ae080501d8c8dce975f86755fdfa375f100
Adrian Velazquez <avelazquez585@gmail.com>
2019-07-10 14:23:27 -0400

    Updates

---
+++ precache-manifest.5baca0237fec8179da49fe3d23cbe269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0c9bd727c3ac83bdd63ae55a41de69",
+    "url": "/React-app-todo/index.html"
+  },
+  {
+    "revision": "0802dc0a0ab6dd8e63f8",
+    "url": "/React-app-todo/static/css/main.aa4cfb6d.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/React-app-todo/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "0802dc0a0ab6dd8e63f8",
+    "url": "/React-app-todo/static/js/main.d7782cf6.chunk.js"
+  },
+  {
+    "revision": "e91b8835490ee09bc849",
+    "url": "/React-app-todo/static/js/runtime~main.5dbd8325.js"
+  }
+]);
